a90dd4ffc7de0e7c7260b2dabb86691a20a03975
ganiyevbekzod <115441938+ganiyevbekzod@users.noreply.github.com>
2023-01-25 04:39:27 +0500

    Add header vs Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Unresolved} from './Components/UnResolved'
 import { Tasks } from './Components/Tasks'
+import { Header } from './Components/Header';
+import { OverwiewHeader } from './Components/Overwiew/OverwiewHeader';
 
 function App() {
   return <div className="App">
+    <Header/>
+    <OverwiewHeader/>
     <Unresolved/>
     <Tasks/>
   </div>;
---
+++ src/Components/Overwiew/OverwiewHeader/OverwiewHeader.jsx
@@ -0,0 +1,27 @@
+import "./OverwiewHeader.scss"
+import SearchLogo from "../../../Assets/Images/search-icon.svg"
+import Reporter from "../../../Assets/Images/notification-icon.svg"
+import User from "../../../Assets/Images/account__user.png"
+export const OverwiewHeader=()=>{
+    return(
+       <div className="OverwiewTop d-flex align-items-center ">
+        <h2 className="Overwiew_title">Overview</h2>
+        <div className="Overwiew_navbar ms-auto">
+        <button className="btn  " >
+        <img className="Overwiew_img" src={SearchLogo} alt="Search_logo" />
+        </button>
+        <button className="btn me-5 ">
+            <img className="Reporter_logo " src={Reporter} alt="Reporter_logo" />
+        </button>
+        <span className="path d-inline "></span>
+        </div>
+        <div className="d-flex align-items-center ">
+        <h5 className="title h5 ms-auto mt-2">Jones Ferdinand</h5>
+        <button className="btn">
+        <img className="User_foto ms-2" src={User} alt="user_foto" />
+        </button>
+        </div>
+       </div>
+    )
+
+}
