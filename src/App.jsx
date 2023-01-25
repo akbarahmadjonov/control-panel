9773d239f2b53c83940dbe00a21ddbe98ce07bb6
ganiyevbekzod <115441938+ganiyevbekzod@users.noreply.github.com>
2023-01-25 06:52:02 +0500

    Add Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { Unresolved} from './Components/UnResolved'
 import { Tasks } from './Components/Tasks'
 import { Header } from './Components/Header';
 import { OverwiewHeader } from './Components/Overwiew/OverwiewHeader';
+import { OverwiewCard } from './Components/Overwiew/OverwiewCard';
+import {OverwiewToday} from "./Components/Overwiew/OverwiewToday";
 
 function App() {
   return <div className="App">
     <Header/>
     <OverwiewHeader/>
-    <Unresolved/>
+    <OverwiewCard/>
+    <OverwiewToday/>
+  <div className='d-flex OverwiewTop justify-content-between mt-5'>
+  <Unresolved/>
     <Tasks/>
+  </div>
   </div>;
 }
 
