55bc5f4bdcddc00378912881f022e4b7ee481ce0
ganiyevbekzod <115441938+ganiyevbekzod@users.noreply.github.com>
2023-01-30 01:28:48 +0500

    Add images to Images,assets to Assets

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
-import NavbarData from "./NavbarData" 
+import {NavbarData} from "./NavbarData" 
 import "./Header.scss"
-import logo from "../../assets/images/logo.svg"
+import logo from "../../Assets/Images/logo.svg"
 export const Header=()=>{
     return(
 <header className="site_header">
